7085797bf32fc7e31b8d74458105649b8eae3c0e
Attainet <msancho@attainet.es>
2024-11-19 10:10:54 +0100

    Maria: permite comas en edicion de oferta

--- public_html/app/Views/comparadorProductos.php
+++ public_html/app/Views/comparadorProductos.php
@@ -84,10 +84,10 @@
                                         <td><?= esc($oferta['ref_producto']) ?></td>
                                         <td><?= esc($oferta['precio']) ?></td>
                                         <td class="star-column text-center" style="border-top: none; border-bottom: none; border-left: none;">
-    <svg class="star-icon <?= $oferta['es_mejor'] ? 'selected' : '' ?>" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-        <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z" />
-    </svg>
-</td>
+                                            <svg class="star-icon <?= $oferta['es_mejor'] ? 'selected' : '' ?>" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                                <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z" />
+                                            </svg>
+                                        </td>
 
 
                                     </tr>
@@ -187,10 +187,18 @@
                 }
             });
         });
-
+        $('#precio').on('input', function() {
+            var value = $(this).val();
+            $(this).val(value.replace(',', '.'));
+        });
         // Enviar el formulario para actualizar la oferta
         $(document).on('submit', '#formEditarOferta', function(event) {
             event.preventDefault(); // Prevenir el envío normal del formulario
+            // Asegurar que cualquier coma en el campo de precio se convierta en un punto antes de enviar
+            var precioField = $('#precio');
+            var value = precioField.val();
+            precioField.val(value.replace(',', '.'));
+
             var formData = $(this).serialize();
 
             // Enviar solicitud POST al controlador
@@ -206,7 +214,6 @@
                 alert("Error al actualizar la oferta. Inténtalo más tarde.");
             });
         });
-
         $('.btn-eliminar').on('click', function() {
             var idOferta = $(this).data('id');
             var idProducto = $(this).data('id-producto');
@@ -251,44 +258,44 @@
                 $('#elegirProveedorModal').modal('show');
             });
         });
-        $('.star-icon').on('click', function () {
-    var $this = $(this);
-    var productoIndex = $this.closest('.selectable-row').data('producto-index');
-    var ofertaIndex = $this.closest('tr').attr('id').split('-').pop();
-    var isSelected = $this.hasClass('selected');
+        $('.star-icon').on('click', function() {
+            var $this = $(this);
+            var productoIndex = $this.closest('.selectable-row').data('producto-index');
+            var ofertaIndex = $this.closest('tr').attr('id').split('-').pop();
+            var isSelected = $this.hasClass('selected');
 
-    if (isSelected) {
-        $this.removeClass('selected');
-        $.ajax({
-            url: '/comparadorProductos/deseleccionarMejor',
-            method: 'POST',
-            data: {
-                productoIndex: productoIndex,
-                ofertaIndex: ofertaIndex
-            },
-            success: function (response) {
-                alert('Proveedor deseleccionado exitosamente');
+            if (isSelected) {
+                $this.removeClass('selected');
+                $.ajax({
+                    url: '/comparadorProductos/deseleccionarMejor',
+                    method: 'POST',
+                    data: {
+                        productoIndex: productoIndex,
+                        ofertaIndex: ofertaIndex
+                    },
+                    success: function(response) {
+                        alert('Proveedor deseleccionado exitosamente');
+                    }
+                });
+            } else {
+                $('tr[data-producto-index="' + productoIndex + '"] .star-icon').removeClass('selected');
+                $this.addClass('selected');
+                $.ajax({
+                    url: '/comparadorProductos/seleccionarMejor',
+                    method: 'POST',
+                    data: {
+                        productoIndex: productoIndex,
+                        ofertaIndex: ofertaIndex
+                    },
+                    success: function(response) {
+                        alert('Proveedor seleccionado exitosamente');
+                        if (!$this.hasClass('selected')) {
+                            $this.addClass('selected');
+                        }
+                    }
+                });
             }
         });
-    } else {
-        $('tr[data-producto-index="' + productoIndex + '"] .star-icon').removeClass('selected');
-        $this.addClass('selected');
-        $.ajax({
-            url: '/comparadorProductos/seleccionarMejor',
-            method: 'POST',
-            data: {
-                productoIndex: productoIndex,
-                ofertaIndex: ofertaIndex
-            },
-            success: function (response) {
-                alert('Proveedor seleccionado exitosamente');
-                if (!$this.hasClass('selected')) {
-                    $this.addClass('selected');
-                }
-            }
-        });
-    }
-});
 
     });
 
