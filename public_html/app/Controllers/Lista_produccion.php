d7a0970f771b747b8bcb5129767cb6a810684872
Attainet <msancho@attainet.es>
2024-11-25 13:46:26 +0100

    Maria add anulador

--- public_html/app/Controllers/Lista_produccion.php
+++ public_html/app/Controllers/Lista_produccion.php
@@ -127,29 +127,23 @@ class Lista_produccion extends BaseControllerGC
 
     public function actualiza_linea($id_lineapedido, $estado)
     {
-        // Conectar a la base de datos
         $data = usuario_sesion();
         $db = db_connect($data['new_db']);
         $Lineaspedido_model = new Lineaspedido_model($db);
         $Lineaspedido_model->actualiza_linea($id_lineapedido, $estado);
-
-        // Conectar a la base de datos
         $data = datos_user();
         $db = db_connect($data['new_db']);
 
-        // Eliminar todos los registros de la tabla procesos_pedidos con la misma id_linea_pedido
         $builder = $db->table('procesos_pedidos');
         $builder->where('id_linea_pedido', $id_lineapedido);
         $builder->delete();
 
-        // Obtener el id_pedido asociado a la línea de pedido
         $builder = $db->table('linea_pedidos');
         $builder->select('id_pedido');
         $builder->where('id_lineapedido', $id_lineapedido);
         $query = $builder->get();
         $id_pedido = $query->getRow()->id_pedido;
 
-        // Comprobar si todas las líneas de pedido asociadas al id_pedido están en estado 5
         $builder = $db->table('linea_pedidos');
         $builder->select('estado');
         $builder->where('id_pedido', $id_pedido);
@@ -161,7 +155,6 @@ class Lista_produccion extends BaseControllerGC
                 break;
             }
         }
-        // Si todas las líneas de pedido están en estado 5, actualizar el estado del pedido a 5
         if ($allInState5) {
             $builder = $db->table('pedidos');
             $builder->set('estado', 5);
@@ -169,7 +162,6 @@ class Lista_produccion extends BaseControllerGC
             $builder->update();
         }
 
-        // Redirigir a la URL de retorno si está definida
         if (isset($_GET['volver'])) {
             $volver = $_GET['volver'];
         }
--- public_html/app/Views/lista_produccion_view.php
+++ public_html/app/Views/lista_produccion_view.php
@@ -167,5 +167,4 @@
         });
     }
 </script>
-
 <?= $this->endSection() ?>
