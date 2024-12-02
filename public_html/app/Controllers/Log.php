a0115f2c814e196e37480466977dd1f0fb3376ce
Attainet <msancho@attainet.es>
2024-12-02 09:49:35 +0100

    maria: Logs elimina

--- public_html/app/Controllers/Log.php
+++ public_html/app/Controllers/Log.php
@@ -20,24 +20,34 @@ class Log extends BaseController
         $db = db_connect($data['new_db']);
         $logModel = new Log_model($db);
         $logs = $logModel->findAll();
-
+    
         if ($logs) {
             return $this->response->setJSON($logs);
         } else {
             return $this->response->setJSON(['error' => 'No se encontraron logs.']);
         }
-
     }
-
-    public function deleteLog($id)
+    public function deleteLog($id_log)
     {
+        if (empty($id_log)) {
+            return $this->response->setJSON(['success' => false, 'message' => 'ID de log no válido.']);
+        }
+    
         $data = datos_user();
         $db = db_connect($data['new_db']);
         $logModel = new Log_model($db);
-        if ($logModel->delete($id)) {
-            return $this->response->setJSON(['success' => true]);
+
+        $log = $logModel->find($id_log);
+        if ($log) {
+            if ($logModel->delete($id_log)) {
+                return $this->response->setJSON(['success' => true]);
+            } else {
+                return $this->response->setJSON(['success' => false, 'message' => 'Error al eliminar el log.']);
+            }
         } else {
-            return $this->response->setJSON(['success' => false, 'message' => 'Error al eliminar el log.']);
+            return $this->response->setJSON(['success' => false, 'message' => 'Log no encontrado para el ID: ' . $id_log]);
         }
     }
+    
+    
 }
