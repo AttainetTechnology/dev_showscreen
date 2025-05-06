e18f1954f1e7cc52edef5f010aa09c5f53ddfed0
Rafa Esteve <resteve@attainet.es>
2025-05-06 10:28:31 +0200

    Hago que se guarde el valor de buenas y el último proceso en la línea de pedido

--- public_html/app/Models/Lineaspedido_model.php
+++ public_html/app/Models/Lineaspedido_model.php
@@ -21,7 +21,9 @@ class Lineaspedido_model extends Model
         'add_linea',
         'total_linea',
         'estado',
-        'escandallo'
+        'escandallo',
+        'ultimo_fichaje',
+        'proceso'
     ];
     public function index() {}
 
